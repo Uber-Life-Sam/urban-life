ae4ba508d0f595e5c210721ebc9660702ceadd45
Uber-Life-Sam <digitalmarketingads.info@gmail.com>
2025-11-16 21:11:30 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import GameHUD from '@/components/game/GameHUD';
 import VirtualJoystick from '@/components/game/VirtualJoystick';
 import InteriorScene from '@/components/game/InteriorScene';
 import JobUI from '@/components/game/JobUI';
-import { usePlayerMovementGTA } from '@/hooks/usePlayerMovementGTA';
+import usePlayerMovementGTA(playerRef, cameraRef);
 import { useCameraOrbit } from '@/hooks/useCameraOrbit';
 import { useCollisionDetection } from '@/hooks/useCollisionDetection';
 import { Button } from '@/components/ui/button';
